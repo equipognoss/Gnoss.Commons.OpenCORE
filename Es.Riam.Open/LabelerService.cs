5959a1d2ea6adea1e463c71ec2935122bf792690
davidruiz-gnoss <davidruiz@gnoss.com>
2022-09-12 11:07:19 +0200

    Service Labeler

---
+++ Es.Riam.Open/LabelerService.cs
@@ -0,0 +1,21 @@
+﻿using Es.Riam.InterfacesOpen;
+using Gnoss.Web.LabelerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Riam.Open
+{
+    public class LabelerService : ILabelerService
+    {
+        public string ObtenerEtiquetas(string pTitulo, string pDescripcion, out string pEtiquetasPropuestas, string pProyectoID, string pDocumentoID, string pExtension = null)
+        {
+            string resultado = "";
+            UtilLabelerService utilLabelerService = new UtilLabelerService();
+            resultado = utilLabelerService.ObtenerEtiquetasDeTituloYDescripcion(pTitulo, pDescripcion, out pEtiquetasPropuestas);
+            return resultado;
+        }
+    }
+}
